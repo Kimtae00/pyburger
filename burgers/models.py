c9d57adde129122760757c34177e8e002aa36385
Kimtaegi <kim76002192@gmail.com>
2024-01-24 15:32:43 +0900

    Update

---
+++ burgers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+# 모델 클래스 정의
+class Burger(models.Model):
+  # 문자열 타입의 버거 이름 정의
+  name = models.CharField(max_length=20)
+  # 정수 타입의 가격, 칼로리 정의
+  price = models.IntegerField(default=0)
+  calories = models.IntegerField(default=0)
+  
--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR / "templates"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "burgers", # 기존 항목들의 맨 위에 기록
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -54,7 +56,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
